0c4ebfafd9edbef001889944dcf9b94a3053e6fc
Sa Sajjad <sqsa93103@gmail.com>
2023-05-20 14:13:19 +0600

    don't say gore

--- sa_upload.sh
+++ sa_upload.sh
@@ -142,4 +142,11 @@ $device-$rom_name has been build successful.
 
 
 ◦•●◉✿ by Sã Śâjjãd ✿◉●•◦" -d "chat_id=$tg_id" -d "parse_mode=HTML"
+#upload on telegram
+ZIP=$(echo *.zip)
+curl -F document=@$ZIP "https://api.telegram.org/bot${bot_api}/sendDocument" \
+-F chat_id="$tg_id" \
+-F "disable_web_page_preview=true" \
+-F "parse_mode=html" \
+-F caption="<code> $res_file_name </code>"
       
