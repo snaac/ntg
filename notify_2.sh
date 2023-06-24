593f5a1f2e7b674a7385f18ae9a8c892930cbfde
Sa Sajjad <sqsa93103@gmail.com>
2023-06-24 23:10:54 +0600

    sourceforge uploader fix

--- notify_2.sh
+++ notify_2.sh
@@ -2,10 +2,10 @@ curl -s "https://api.telegram.org/bot${bot_api}/sendmessage" -d "text=
 ◦•●◉✿ 🅑🅤🅘🅛🅓 🅢🅤🅒🅒🅔🅔🅓 ✿◉●•◦
       
       
-File Name: <code>$file_name</code>
+File Name: <code>$dlink</code>
 
 
-Download Link: <a href='https://sourceforge.net/projects/snx/files/$device/$rom_name/$dlink/download'>Here</a>
+Download Link: <a href='https://sourceforge.net/projects/snx/files/$device/$dlink/download'>Here</a>
 
 
 ◦•●◉✿ by Sã Śâjjãd ✿◉●•◦" -d "chat_id=$tg_id" -d "parse_mode=HTML"
