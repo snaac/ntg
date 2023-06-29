25e86bf595b6e8ef0bb22dabb4bbed2d5f5b0a85
Sa Sajjad <sqsa93103@gmail.com>
2023-06-29 22:48:11 +0600

    try again

--- test.sh
+++
@@ -1,3 +0,0 @@
-cd ~/$rom_name
-cd out/target/product/$device/apex/com.android.runtime/lib64
-bash <(curl -s https://raw.githubusercontent.com/snaac/ntg/$CIRRUS_BRANCH/uploading.sh) -f libart-compiler.so
